bf49667b7192085a189f0f5f0e562a50f62795d8
famedst <132010122+famedst@users.noreply.github.com>
2023-07-25 22:45:00 -0600

    Add files via upload

---
+++ Sleep_mode.X/build/default/production/main_sleep.p1.d
@@ -0,0 +1,2 @@
+build/default/production/main_sleep.p1:  \
+main_sleep.c 
---
+++ Sleep_mode.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Sleep_mode.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Sleep_mode.X.production.hex
